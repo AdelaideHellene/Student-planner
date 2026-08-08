df19af021c345c69bc5ffe16e9bf1645d300902e
Adelaide Hellene <adel.hellene@icloud.com>
2026-08-08 10:52:32 +0400

    Complete javascript for splash screen

--- Student planner/js/splash.js
+++ Student planner/js/splash.js
@@ -1,6 +1,23 @@
 const startButton = document.getElementById("start-button");
+const logo = document.querySelector(".splash-logo");
 
-startButton.addEventListener("click", function(){
+
+setTimeout(() => {
+
+    logo.style.opacity = "1";
+    logo.style.transform = "scale(1)";
+
+}, 300);
+
+
+setTimeout(() => {
+
+    startButton.style.opacity = "1";
+
+}, 1800);
+
+
+startButton.addEventListener("click", function() {
 
     window.location.href = "login.html";
 
