b3e0264128d586516e9c4dc4a5e9f6d66e155292
Adelaide Hellene <adel.hellene@icloud.com>
2026-08-09 16:15:38 +0400

    Fix splash screen animation

--- js/splash.js
+++ js/splash.js
@@ -1,24 +1,14 @@
 const startButton = document.getElementById("start-button");
 const logo = document.querySelector(".splash-logo");
 
-
 setTimeout(() => {
-
-    logo.style.opacity = "1";
-    logo.style.transform = "scale(1)";
-
+    logo.classList.add("show");
 }, 300);
 
-
 setTimeout(() => {
-
-    startButton.style.opacity = "1";
-
+    startButton.classList.add("show");
 }, 1800);
 
-
-startButton.addEventListener("click", function() {
-
+startButton.addEventListener("click", function () {
     window.location.href = "login.html";
-
 });
